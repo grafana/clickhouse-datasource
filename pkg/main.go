3d5f9e8350242426e9629fa3120ed0bfac5870ca
Scott Lepper <scott.lepper@gmail.com>
2021-11-19 14:04:35 -0500

    update import

--- pkg/main.go
+++ pkg/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"os"
 
+	"github.com/grafana/clickhouse-datasource/pkg/plugin"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-	"github.com/grafana/grafana-starter-datasource-backend/pkg/plugin"
 	"github.com/grafana/sqlds"
 )
 
