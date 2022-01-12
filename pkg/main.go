6390af91705d4e915e76026e7237f98438b0bbae
Scott Lepper <scott.lepper@gmail.com>
2022-01-12 10:01:39 -0500

    fix issue using wrong instance

--- pkg/main.go
+++ pkg/main.go
@@ -4,15 +4,21 @@ import (
 	"os"
 
 	"github.com/grafana/clickhouse-datasource/pkg/plugin"
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/grafana/sqlds"
 )
 
 func main() {
-	ds := sqlds.NewDatasource(&plugin.Clickhouse{})
-	if err := datasource.Manage("grafana-clickhouse-datasource", ds.NewDatasource, datasource.ManageOpts{}); err != nil {
+	if err := datasource.Manage("grafana-clickhouse-datasource", newDatasource, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func newDatasource(settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	ds := sqlds.NewDatasource(&plugin.Clickhouse{})
+	return ds.NewDatasource(settings)
 }
