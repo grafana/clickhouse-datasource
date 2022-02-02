2fb6a2ac4ff87c158fb59957027454de35d080d5
Scott Lepper <scott.lepper@gmail.com>
2022-02-02 16:50:32 -0500

    fix after ast change

--- src/components/editor.ts
+++ src/components/editor.ts
@@ -6,10 +6,10 @@ export const getFormat = (sql: string): Format => {
   // convention to format as time series
   // first field as "time" alias and requires at least 2 fields (time and metric)
   const ast = sqlToAST(sql);
-  const select = ast.get('SELECT');
-  if (isString(select)) {
+  const selectList = ast.get('SELECT') || [];
+  if (selectList.length > 0 && isString(selectList[0])) {
     // remove function parms that may contain commas
-    const cleanSelect = select.replace(/\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)/, '');
+    const cleanSelect = selectList[0].replace(/\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)/, '');
     const fields = cleanSelect.split(',');
     if (fields.length > 1) {
       return fields[0].toLowerCase().endsWith('as time') ? Format.TIMESERIES : Format.TABLE;
