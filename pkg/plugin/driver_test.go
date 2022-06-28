44c36aef1c37329576587a2f99cd256b38351e8e
Andrew Hackmann <ajhack93@gmail.com>
2022-06-28 16:04:17 -0500

    test containers

--- pkg/plugin/driver_test.go
+++ pkg/plugin/driver_test.go
@@ -1,11 +1,16 @@
 package plugin
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestConnect(t *testing.T) {
@@ -28,4 +33,35 @@ func TestConnectSecure(t *testing.T) {
 		_, err := clickhouse.Connect(settings, json.RawMessage{})
 		assert.Equal(t, nil, err)
 	})
+}
+
+func TestMain(m *testing.M) {
+	// create a ClickHouse container
+	ctx := context.Background()
+	_, err := os.Getwd()
+	if err != nil {
+		// can't test without container
+		panic(err)
+	}
+
+	// for now, we test against a hardcoded database-server version but we should make this a property
+	req := testcontainers.ContainerRequest{
+		Image:        fmt.Sprintf("clickhouse/clickhouse-server"),
+		ExposedPorts: []string{"9000/tcp"},
+		WaitingFor:   wait.ForLog("Ready for connections"),
+	}
+	clickhouseContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		// can't test without container
+		panic(err)
+	}
+
+	p, _ := clickhouseContainer.MappedPort(ctx, "9000")
+
+	os.Setenv("CLICKHOUSE_DB_PORT", p.Port())
+	defer clickhouseContainer.Terminate(ctx) //nolint
+	os.Exit(m.Run())
 }
