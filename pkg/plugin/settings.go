6a1ef93baba65a6fe258ce94bf290ceb46b67c55
Andreas Christou <andreas.christou@grafana.com>
2023-02-06 18:15:08 +0000

    Validate all variables

--- pkg/plugin/settings.go
+++ pkg/plugin/settings.go
@@ -3,6 +3,7 @@ package plugin
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
@@ -11,7 +12,7 @@ import (
 // Settings - data loaded from grafana settings database
 type Settings struct {
 	Server             string `json:"server,omitempty"`
-	Port               int64  `json:"port,string,omitempty"`
+	Port               int64  `json:"port,omitempty"`
 	Username           string `json:"username,omitempty"`
 	DefaultDatabase    string `json:"defaultDatabase,omitempty"`
 	InsecureSkipVerify bool   `json:"tlsSkipVerify,omitempty"`
@@ -39,9 +40,79 @@ func (settings *Settings) isValid() (err error) {
 
 // LoadSettings will read and validate Settings from the DataSourceConfig
 func LoadSettings(config backend.DataSourceInstanceSettings) (settings Settings, err error) {
-	if err := json.Unmarshal(config.JSONData, &settings); err != nil {
+	var jsonData map[string]interface{}
+	if err := json.Unmarshal(config.JSONData, &jsonData); err != nil {
 		return settings, fmt.Errorf("%s: %w", err.Error(), ErrorMessageInvalidJSON)
 	}
+
+	if jsonData["server"] != nil {
+		settings.Server = jsonData["server"].(string)
+	}
+	if jsonData["port"] != nil {
+		if port, err := strconv.ParseInt(jsonData["port"].(string), 0, 64); err == nil {
+			settings.Port = port
+		} else {
+			return settings, fmt.Errorf("could not parse Port value: %w", err)
+		}
+	}
+	if jsonData["username"] != nil {
+		settings.Username = jsonData["username"].(string)
+	}
+	if jsonData["defaultDatabase"] != nil {
+		settings.DefaultDatabase = jsonData["defaultDatabase"].(string)
+	}
+
+	if jsonData["tlsSkipVerify"] != nil {
+		if tlsSkipVerify, ok := jsonData["tlsSkipVerify"].(string); ok {
+			settings.InsecureSkipVerify, err = strconv.ParseBool(tlsSkipVerify)
+			if err != nil {
+				return settings, fmt.Errorf("could not parse tlsSkipVerify value: %w", err)
+			}
+		} else {
+			settings.InsecureSkipVerify = jsonData["tlsSkipVerify"].(bool)
+		}
+	}
+	if jsonData["tlsAuth"] != nil {
+		if tlsAuth, ok := jsonData["tlsAuth"].(string); ok {
+			settings.TlsClientAuth, err = strconv.ParseBool(tlsAuth)
+			if err != nil {
+				return settings, fmt.Errorf("could not parse tlsAuth value: %w", err)
+			}
+		} else {
+			settings.TlsClientAuth = jsonData["tlsAuth"].(bool)
+		}
+	}
+	if jsonData["tlsAuthWithCACert"] != nil {
+		if tlsAuthWithCACert, ok := jsonData["tlsAuthWithCACert"].(string); ok {
+			settings.TlsAuthWithCACert, err = strconv.ParseBool(tlsAuthWithCACert)
+			if err != nil {
+				return settings, fmt.Errorf("could not parse tlsAuthWithCACert value: %w", err)
+			}
+		} else {
+			settings.TlsAuthWithCACert = jsonData["tlsAuthWithCACert"].(bool)
+		}
+	}
+	if jsonData["secure"] != nil {
+		if secure, ok := jsonData["secure"].(string); ok {
+			settings.Secure, err = strconv.ParseBool(secure)
+			if err != nil {
+				return settings, fmt.Errorf("could not parse secure value: %w", err)
+			}
+		} else {
+			settings.Secure = jsonData["secure"].(bool)
+		}
+	}
+
+	if jsonData["timeout"] != nil {
+		settings.Timeout = jsonData["timeout"].(string)
+	}
+	if jsonData["queryTimeout"] != nil {
+		settings.QueryTimeout = jsonData["queryTimeout"].(string)
+	}
+	if jsonData["protocol"] != nil {
+		settings.Protocol = jsonData["protocol"].(string)
+	}
+
 	if strings.TrimSpace(settings.Timeout) == "" {
 		settings.Timeout = "10"
 	}
