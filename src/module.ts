c7a05a5aadea03fcbea91535520735f92309202d
Alyssa Bull <58453566+alyssabull@users.noreply.github.com>
2023-04-09 14:48:22 -0600

    Add initial feature tracking (#350)

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,32 @@
-import { DataSourcePlugin } from '@grafana/data';
+import { DataSourcePlugin, DashboardLoadedEvent } from '@grafana/data';
 import { Datasource } from './data/CHDatasource';
 import { ConfigEditor } from './views/CHConfigEditor';
 import { CHQueryEditor } from './views/CHQueryEditor';
 import { CHQuery, CHConfig } from './types';
+import { getAppEvents } from '@grafana/runtime';
+import { analyzeQueries, trackClickhouseDashboardLoaded } from 'tracking';
+import pluginJson from './plugin.json';
 
 export const plugin = new DataSourcePlugin<Datasource, CHQuery, CHConfig>(Datasource)
   .setConfigEditor(ConfigEditor)
   .setQueryEditor(CHQueryEditor);
+
+// Track dashboard loads to RudderStack
+getAppEvents().subscribe<DashboardLoadedEvent<CHQuery>>(
+  DashboardLoadedEvent,
+  ({ payload: { dashboardId, orgId, grafanaVersion, queries } }) => {
+    
+    const clickhouseQueries = queries[pluginJson.id]?.filter((q) => !q.hide);
+    if (!clickhouseQueries?.length) {
+      return;
+    }
+
+    trackClickhouseDashboardLoaded({
+      clickhouse_plugin_version: pluginJson.info.version,
+      grafana_version: grafanaVersion,
+      dashboard_id: dashboardId,
+      org_id: orgId,
+      ...analyzeQueries(clickhouseQueries),
+    });
+  }
+);
---
+++ src/tracking.ts
@@ -0,0 +1,53 @@
+import { reportInteraction } from '@grafana/runtime';
+import { BuilderMode, CHQuery, QueryType } from 'types';
+
+export const trackClickhouseDashboardLoaded = (props: ClickhouseDashboardLoadedProps) => {
+  reportInteraction('grafana_ds_clickhouse_dashboard_loaded', props);
+};
+
+export type ClickhouseCounters = {
+  sql_queries: number,
+  builder_queries: number,
+  builder_table_queries: number,
+  builder_aggregate_queries: number,
+  builder_time_series_queries: number
+};
+
+export interface ClickhouseDashboardLoadedProps extends ClickhouseCounters {
+  clickhouse_plugin_version?: string;
+  grafana_version?: string;
+  dashboard_id: string;
+  org_id?: number;
+  [key: string]: any;
+};
+
+export const analyzeQueries = (queries: CHQuery[]): ClickhouseCounters => {
+  const counters = {
+    sql_queries: 0,
+    builder_queries: 0,
+    builder_table_queries: 0,
+    builder_aggregate_queries: 0,
+    builder_time_series_queries: 0
+  };
+  
+  queries.forEach((query) => {
+    switch (query.queryType) {
+      case (QueryType.SQL):
+        counters.sql_queries++;
+        break;
+      case (QueryType.Builder):
+        counters.builder_queries++;
+        if (query.builderOptions.mode === BuilderMode.Aggregate) {
+          counters.builder_aggregate_queries++;
+        } else if (query.builderOptions.mode === BuilderMode.List) {
+          counters.builder_table_queries++;
+        } else if (query.builderOptions.mode === BuilderMode.Trend) {
+          counters.builder_time_series_queries++;
+        }
+        break;
+    }
+  });
+
+  return counters;
+};
+
