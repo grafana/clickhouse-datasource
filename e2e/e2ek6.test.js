6c2b107ec9c37c7570b2d849ed855e63389ddfff
alyssabull <alyssabull@gmail.com>
2023-06-06 15:50:02 -0600

    Import @grafana/e2e-selectors

--- e2e/e2ek6.test.js
+++ e2e/e2ek6.test.js
@@ -1,28 +1,28 @@
 import { chromium } from 'k6/experimental/browser';
 import { check, fail } from 'k6';
 import http from 'k6/http';
+
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 import { selectors } from 'unpkg.com/@grafana/e2e-selectors/dist/index.js';
 
-console.log('selectors', selectors);
-
-http.setResponseCallback(http.expectedStatuses({ min: 200, max: 400 }));
 const dashboardTitle = `e2e-test-dashboard-${uuidv4()}`;
 
 export async function login(page) {
   try {
-    await page.goto('http://localhost:3000/login', { waitUntil: 'networkidle' });
+    const loginURL = selectors.pages.Login.url;
+    await page.goto(`http://localhost:3000${loginURL}`, { waitUntil: 'networkidle' });
 
-    // e2e.flows.login({ username: 'admin', password: 'admin'})
-
-    page.locator('input[name="user"]').type('admin');
-    page.locator('input[name="password"]').type('admin');
-    page.locator('button[type="submit"]').click();
+    const usernameInput = page.locator(`input[aria-label="${selectors.pages.Login.username}"]`)
+    await usernameInput.type('admin');
+    const passwordInput = page.locator(`input[aria-label="${selectors.pages.Login.password}"]`)
+    await passwordInput.type('admin');
+    const submitButton = page.locator(`button[aria-label="${selectors.pages.Login.submit}"]`)
+    await submitButton.click();
 
     // checks page for skip change password screen
     check(page, {
       'change password is presented':
-        page.locator('button[aria-label="Skip change password button"]').textContent() === 'Skip',
+        page.locator(`button[aria-label="${selectors.pages.Login.skip}"]`).textContent() === 'Skip',
     });
   } catch (e) {
     fail(`login failed: ${e}`);
@@ -31,14 +31,19 @@ export async function login(page) {
 
 export async function addDatasource(page) {
   try {
-    await page.goto('http://localhost:3000/connections/datasources/new', { waitUntil: 'networkidle' });
-    const findDatasource = page.locator('input[placeholder="Filter by name or type"]');
-    await findDatasource.type('click');
-    const clickHouseDataSource = page.locator('button[aria-label="Add new data source ClickHouse"]');
+    const addDataSourceURL = selectors.pages.AddDataSource.url;
+    await page.goto(`http://localhost:3000${addDataSourceURL}`, { waitUntil: 'networkidle' });
+    
+    const clickHouseDataSource = page.locator(`button[aria-label="${selectors.pages.AddDataSource.dataSourcePluginsV2('ClickHouse')}"]`);
     await clickHouseDataSource.click();
-    page.locator(`input[aria-label="Server address"]`).type('localhost');
-    page.locator('input[aria-label="Server port"]').type('9000');
-    const saveAndTestButton = page.locator('button[data-testid="data-testid Data source settings page Save and Test button"]');
+    const dataSourceName = page.locator(`input[aria-label="${selectors.pages.DataSource.name}"]`);
+    dataSourceName.fill('');
+    dataSourceName.type(`ClickHouse-e2e-test-${uuidv4()}`);
+    const serverAddress = page.locator(`input[aria-label="Server address"]`);
+    serverAddress.type('localhost');
+    const serverPort = page.locator('input[aria-label="Server port"]');
+    serverPort.type('9000');
+    const saveAndTestButton = page.locator(`button[data-testid="data-testid ${selectors.pages.DataSource.saveAndTest}"]`);
     await saveAndTestButton.click();
 
     // checks the page for the data source is working message
@@ -63,21 +68,23 @@ export async function addDatasource(page) {
 
 export async function addDashboard(page) {
   try {
-    await page.goto('http://localhost:3000/dashboard/new', { waitUntil: 'networkidle' });
-    const dashboardSettings = page.locator('button[aria-label="Dashboard settings"]');
-    await dashboardSettings.click();
-    const dashboardTitleInput = page.locator('#title-input');
+    const addDashboardURL = selectors.pages.AddDashboard.url;
+    await page.goto(`http://localhost:3000${addDashboardURL}`, { waitUntil: 'networkidle' });
+    
+    const saveDashboardToolbarButton = page.locator(`button[aria-label="${selectors.components.PageToolbar.item('Save dashboard')}"]`);
+    await saveDashboardToolbarButton.click();
+    const dashboardTitleInput = page.locator(`input[aria-label="${selectors.pages.SaveDashboardAsModal.newName}"]`);
     dashboardTitleInput.fill('');
     await dashboardTitleInput.type(dashboardTitle);
-    const saveDashboardButton = page.locator('button[aria-label="Dashboard settings aside actions Save button"]');
-    saveDashboardButton.click();
-    const saveDashboardSidebarButton = page.locator('button[aria-label="Save dashboard button"]');
-    saveDashboardSidebarButton.click();
+    const saveDashboardModalButton = page.locator(`button[aria-label="${selectors.pages.SaveDashboardAsModal.save}"]`);
+    saveDashboardModalButton.click();
 
     // checks that query is run successfully
     check(page, {
-      'query builder dashboard created successfully':
+      'dashboard created successfully':
       page.locator(`span[data-testid="data-testid ${dashboardTitle} breadcrumb"]`).textContent() === `${dashboardTitle}`,
+      // TODO: replace above test with below test that aligns with core Grafana flows
+      // page.locator(`div[data-testid="data-testid ${selectors.components.Alert.alertV2('success')}`).textContent() === 'Dashboard saved',
     })
   } catch(e) {
     fail(`add dashboard failed: ${e}`);
@@ -87,6 +94,7 @@ export async function addDashboard(page) {
 export async function configurePanel(browser, page) {
   try {
     const latestDashboardURL = page.url();
+
     await page.goto(`${latestDashboardURL}`, { waitUntil: 'networkidle' });
     const addPanelButton = page.locator('button[aria-label="Add new panel"]');
     await addPanelButton.click();
@@ -108,95 +116,7 @@ export async function configurePanel(browser, page) {
     const runQueryButton = page.locator('button[data-testid="data-testid RefreshPicker run button"]');
     await runQueryButton.click();
 
-    // TODO: export to util file
-    let data = {
-      "queries": [
-          {
-              "datasource": {
-                  "type": "grafana-clickhouse-datasource",
-                  "uid": `${latestDashboardUID}`
-              },
-              "refId": "A",
-              "queryType": "builder",
-              "rawSql": "SELECT memory_usage, event_time FROM system.\"query_log\" WHERE   ( event_time  >= $__fromTime AND event_time <= $__toTime ) LIMIT 100",
-              "builderOptions": {
-                  "mode": "list",
-                  "fields": [
-                      "memory_usage",
-                      "event_time"
-                  ],
-                  "limit": 100,
-                  "database": "system",
-                  "table": "query_log",
-                  "filters": [
-                      {
-                          "operator": "WITH IN DASHBOARD TIME RANGE",
-                          "filterType": "custom",
-                          "key": "event_time",
-                          "type": "datetime",
-                          "condition": "AND",
-                          "restrictToFields": [
-                              {
-                                  "name": "event_time",
-                                  "type": "DateTime",
-                                  "label": "event_time",
-                                  "picklistValues": []
-                              },
-                              {
-                                  "name": "event_time_microseconds",
-                                  "type": "DateTime64(6)",
-                                  "label": "event_time_microseconds",
-                                  "picklistValues": []
-                              },
-                              {
-                                  "name": "query_start_time",
-                                  "type": "DateTime",
-                                  "label": "query_start_time",
-                                  "picklistValues": []
-                              },
-                              {
-                                  "name": "query_start_time_microseconds",
-                                  "type": "DateTime64(6)",
-                                  "label": "query_start_time_microseconds",
-                                  "picklistValues": []
-                              },
-                              {
-                                  "name": "initial_query_start_time",
-                                  "type": "DateTime",
-                                  "label": "initial_query_start_time",
-                                  "picklistValues": []
-                              },
-                              {
-                                  "name": "initial_query_start_time_microseconds",
-                                  "type": "DateTime64(6)",
-                                  "label": "initial_query_start_time_microseconds",
-                                  "picklistValues": []
-                              }
-                          ]
-                      }
-                  ],
-                  "orderBy": []
-              },
-              "meta": {
-                  "timezone": "America/Denver"
-              },
-              "datasourceId": 2922,
-              "intervalMs": 43200000,
-              "maxDataPoints": 531
-          }
-      ],
-      "from": "1654534659803",
-      "to": "1686070659803"
-  }
-
-  // Using a JSON string as body
-  let res = http.post(url, JSON.stringify(data), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  http.post('http://localhost:3000/api/ds/query?ds_type=grafana-clickhouse-datasource');
-
-    check(res, {
+    check(page, {
       'configure panel query returns a status of 200':
       (r) => r.status === 200,
     })
