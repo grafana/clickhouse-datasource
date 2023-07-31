920d433d50ccde0833a4def407067f05bb9ac97f
Adam Simpson <adam@adamsimpson.net>
2023-07-31 16:52:47 -0400

    Fix auto formatting by reverting to table correctly (#469)

--- src/components/editor.ts
+++ src/components/editor.ts
@@ -8,7 +8,7 @@ export const getFormat = (sql: string, selectedFormat: Format): Format => {
     // first field as "time" alias and requires at least 2 fields (time and metric)
     const selectList = getFields(sql);
     // if there are more than 2 fields, index 1 will be a ','
-    if (selectList.length > 2 && isString(selectList[0])) {
+    if (selectList.length >= 2 && isString(selectList[0])) {
       const firstProjection = selectList[0].trim().toLowerCase();
       if (firstProjection.endsWith('as time')) {
         return Format.TIMESERIES;
