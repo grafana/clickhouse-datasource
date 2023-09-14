704fd6a1dd7a7190b21e584a2b834a0ae567dcae
Andrew Hackmann <ajhack93@gmail.com>
2023-09-14 11:56:51 -0500

    stuff

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -25,10 +25,6 @@ var runTests = &cobra.Command{
 
 var testFailures = 0
 
-var platforms = []dagger.Platform{
-	"linux/amd64",
-}
-
 func e2eTests(cmd *cobra.Command, args []string) {
 	ctx := cmd.Context()
 
@@ -39,57 +35,65 @@ func e2eTests(cmd *cobra.Command, args []string) {
 	}
 	defer client.Close()
 
-	for _, platform := range platforms {
-		clickHouseAssets := buildPlugin(ctx, client, platform)
-		grafanaContainer := startGrafana(client, clickHouseAssets, platform, ctx)
-		clickHouseContainer := startClickHouse(client, ctx)
-		fmt.Println("Plugin built")
+	clickHouseAssets := buildPlugin(ctx, client)
+	grafanaContainer := startGrafana(client, clickHouseAssets)
 
-		// run e2e tests
-		fmt.Println("Starting k6 tests")
-		runk6(client, ctx, grafanaContainer, clickHouseContainer, clickHouseAssets)
-		fmt.Println("k6 tests ran")
+	// entries, err := clickHouseAssets.Directory("./dist").Entries(ctx)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-		//check if e2e tests pass
-		fmt.Println("Checking test summary")
-		j, _ := os.ReadFile("test_summary.json")
+	// fmt.Println(entries)
 
-		data := struct {
-			RootGroup struct {
-				Checks []struct {
-					Name   string `json:"name"`
-					Passes int    `json:"passes"`
-					Fails  int    `json:"fails"`
-				}
-			} `json:"root_group"`
-		}{}
-		err = json.Unmarshal(j, &data)
-		if err != nil {
-			fmt.Println("Cannot unmarshal the json", err)
-			return
-		}
-		for _, check := range data.RootGroup.Checks {
-			if check.Fails > 0 {
-				testFailures++
-				fmt.Println("Test failed:", check.Name)
-				continue
+	clickHouseContainer := startClickHouse(client, ctx)
+	fmt.Println("Plugin built")
+
+	// run e2e tests
+	fmt.Println("Starting k6 tests")
+	runk6(client, ctx, grafanaContainer, clickHouseContainer, client.Host().File("e2e/e2ek6.test.js"))
+	fmt.Println("k6 tests ran")
+
+	//check if e2e tests pass
+	fmt.Println("Checking test summary")
+	j, _ := os.ReadFile("test_summary.json")
+
+	data := struct {
+		RootGroup struct {
+			Checks []struct {
+				Name   string `json:"name"`
+				Passes int    `json:"passes"`
+				Fails  int    `json:"fails"`
 			}
-			fmt.Println("Test passed:", check.Name)
-		}
-		fmt.Println("Test summary check complete")
+		} `json:"root_group"`
+	}{}
+	err = json.Unmarshal(j, &data)
+	if err != nil {
+		fmt.Println("Cannot unmarshal the json", err)
+		return
 	}
+	for _, check := range data.RootGroup.Checks {
+		if check.Fails > 0 {
+			testFailures++
+			fmt.Println("Test failed:", check.Name)
+			continue
+		}
+		fmt.Println("Test passed:", check.Name)
+	}
+	fmt.Println("Test summary check complete")
 }
 
-func buildPlugin(ctx context.Context, client *dagger.Client, platform dagger.Platform) *dagger.Container {
+func buildPlugin(ctx context.Context, client *dagger.Client) *dagger.Directory {
 	fmt.Println("Building plugin")
-	backend := buildBackend(ctx, client, client.Host().Directory(".", dagger.HostDirectoryOpts{Exclude: []string{"node_modules/**", "dist/**", "provisioning/**"}}), platform)
+	backend := buildBackend(ctx, client, client.Host().Directory(".", dagger.HostDirectoryOpts{Exclude: []string{"node_modules/**", "dist/**", "provisioning/**"}}))
 
-	return WithYarnDependencies(client, backend, platform)
+	frontEndDependencies := WithYarnDependencies(client, backend)
+	return BuildFrontEnd(client, frontEndDependencies).Directory("./dist")
 }
 
-func buildBackend(ctx context.Context, client *dagger.Client, directory *dagger.Directory, platform dagger.Platform) *dagger.Container {
+func buildBackend(ctx context.Context, client *dagger.Client, directory *dagger.Directory) *dagger.Container {
 	container := client.
-		Container(dagger.ContainerOpts{Platform: platform}).
+		Container().
 		From("golang:1.21.0").
 		WithWorkdir("./clickhouse-datasource").
 		WithDirectory(".", directory).
@@ -99,14 +103,14 @@ func buildBackend(ctx context.Context, client *dagger.Client, directory *dagger.
 	return container
 }
 
-func WithYarnDependencies(client *dagger.Client, container *dagger.Container, platform dagger.Platform) *dagger.Container {
+func WithYarnDependencies(client *dagger.Client, container *dagger.Container) *dagger.Container {
 
 	var (
 		packageJSON = container.File("package.json")
 		yarnLock    = container.File("yarn.lock")
 	)
 
-	nodeModules := client.Container(dagger.ContainerOpts{Platform: platform}).
+	nodeModules := client.Container().
 		From("node:16.13.2").
 		WithWorkdir("/src").
 		WithFile("/src/package.json", packageJSON).
@@ -118,23 +122,33 @@ func WithYarnDependencies(client *dagger.Client, container *dagger.Container, pl
 	return container.WithDirectory("node_modules", nodeModules)
 }
 
-func startGrafana(client *dagger.Client, clickHouseAssets *dagger.Container, platform dagger.Platform, ctx context.Context) *dagger.Container {
+func BuildFrontEnd(client *dagger.Client, container *dagger.Container) *dagger.Container {
+	// [.config .eslintrc .git .github .gitignore .prettierrc.js .vscode CHANGELOG.md CONTRIBUTING.md DEV_GUIDE.md LICENSE Magefile.go README.md config config-secure cspell.config.json cypress-e2e dist docker-compose
+	//┃  go.mod go.sum jest-runner-serial.js jest-setup.js jest.config.js node_modules package.json pkg provisioning scripts src tsconfig.json yarn.lock]
+
+	// [.config .eslintrc .git .github .gitignore .prettierrc.js .vscode CHANGELOG.md CONTRIBUTING.md DEV_GUIDE.md LICENSE Magefile.go README.md config config-secure cspell.config.json cypress-e2e dist docker-compose
+	//┃  go.mod go.sum jest-runner-serial.js jest-setup.js jest.config.js node_modules package.json pkg provisioning scripts src tsconfig.json yarn.lock]
+	//return container.WithExec([]string{"yarn", "build"})
+	dist := client.Container().
+		From("node:16.13.2").
+		WithWorkdir("/src").
+		WithDirectory("/src", container.Directory(".")).
+		WithExec([]string{"yarn", "build"}).
+		Directory("/src/dist")
+
+	return container.WithDirectory("dist", dist)
+}
+
+func startGrafana(client *dagger.Client, clickHouseDist *dagger.Directory) *dagger.Container {
 	fmt.Println("Building Grafana")
 	container := client.
-		Container(dagger.ContainerOpts{Platform: platform}).
+		Container().
 		From("grafana/grafana:latest").
-		WithFile("grafana/conf/custom.ini", client.Host().File("e2e/custom.ini")).
-		WithDirectory("/data/plugins", clickHouseAssets.Directory(".")).
+		WithFile("conf/custom.ini", client.Host().File("e2e/custom.ini")).
+		WithDirectory("/data/plugins/clickhouse-datasource", clickHouseDist).
 		WithExec(nil).
 		//WithEnvVariable("P", "./plugins"). //point to clickhouseassets directory
 		WithExposedPort(3000)
-
-	// entries, err := container.Directory(".").Entries(ctx)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-
-	// fmt.Println(entries)
 
 	fmt.Println("Grafana built")
 
@@ -153,16 +167,16 @@ func startClickHouse(client *dagger.Client, ctx context.Context) *dagger.Contain
 	return container
 }
 
-func runk6(client *dagger.Client, ctx context.Context, grafanaContainer *dagger.Container, clickhouseContainer *dagger.Container, clickHouseAssets *dagger.Container) {
+func runk6(client *dagger.Client, ctx context.Context, grafanaContainer *dagger.Container, clickhouseContainer *dagger.Container, testFile *dagger.File) {
 	value, err := client.
 		Container().
 		From("grafana/k6:master-with-browser").
 		WithServiceBinding("clickhouse", clickhouseContainer). //dns for this? e.g. does grafana connect to localhost:9000 or "clickhouse:9000"
 		WithServiceBinding("grafana", grafanaContainer).
-		WithDirectory(".", clickHouseAssets.Directory(".")).
 		WithEnvVariable("K6_BROWSER_ARGS", "no-sandbox").
+		WithFile("k6.test.js", testFile).
 		// WithEnvVariable("K6_BROWSER_HEADLESS", "0").
-		WithExec([]string{"run", "e2e/e2ek6.test.js"}, dagger.ContainerWithExecOpts{InsecureRootCapabilities: true}).Stderr(ctx)
+		WithExec([]string{"run", "k6.test.js"}, dagger.ContainerWithExecOpts{InsecureRootCapabilities: true}).Stderr(ctx)
 	if err != nil {
 		fmt.Println(err)
 		return
