1051c175268fec2b4b73defdf2eaddffc77caff1
Spencer Torres <contact@spencertorres.com>
2023-12-04 02:24:06 -0500

    add test for isBuilderOptionsRunnable func

--- src/data/utils.test.ts
+++ src/data/utils.test.ts
@@ -1,4 +1,32 @@
-import { columnLabelToPlaceholder } from "./utils";
+import { QueryBuilderOptions, QueryType } from "types/queryBuilder";
+import { columnLabelToPlaceholder, isBuilderOptionsRunnable } from "./utils";
+
+describe('isBuilderOptionsRunnable', () => {
+  it('should return false for empty builder options', () => {
+    const opts: QueryBuilderOptions = {
+      database: 'default',
+      table: 'test',
+      queryType: QueryType.Table
+    };
+
+    const runnable = isBuilderOptionsRunnable(opts);
+    expect(runnable).toBe(false);
+  });
+
+  it('should return true for valid builder options', () => {
+    const opts: QueryBuilderOptions = {
+      database: 'default',
+      table: 'test',
+      queryType: QueryType.Table,
+      columns: [
+        { name: 'valid_column' }
+      ]
+    };
+
+    const runnable = isBuilderOptionsRunnable(opts);
+    expect(runnable).toBe(true);
+  });
+});
 
 describe('columnLabelToPlaceholder', () => {
   it('converts to lowercase and removes multiple spaces', () => {
