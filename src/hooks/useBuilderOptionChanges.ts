e2c61da12d995aafe44881db6f33247367894cd7
Spencer Torres <contact@spencertorres.com>
2023-12-05 00:31:19 -0500

    simplify useBuilderOptionChanges hook, add test

--- src/hooks/useBuilderOptionChanges.ts
+++ src/hooks/useBuilderOptionChanges.ts
@@ -1,32 +1,24 @@
 import React from 'react';
 
-type onOptionChangeFn<T> = (key: keyof T | Object) => (nextValue: React.SetStateAction<any>) => void;
+type onOptionChangeFn<T> = (key: keyof T) => (nextValue: React.SetStateAction<any>) => void;
 
 /**
  * Returns a function that can apply changes with an object or a specific key in an object. When called
  * will run another function with the changes applied.
  * 
- * (Does not deep clone the object, for now)
+ * Does not deep clone the object. This is used for top level fields on the QueryBuilderOptions type.
  * 
  * @param onChange a function that receives the updated state from the change function
  * @param prevState the current (previous) state object
  * @returns a function used to apply changes to individual fields
  */
 export function useBuilderOptionChanges<T>(onChange: (nextState: T) => void, prevState: T): onOptionChangeFn<T> {
-  return (key: keyof T | Object) =>
+  return (key: keyof T) =>
     (nextValue: React.SetStateAction<any>) => {
-    let nextState: T;
-    if (typeof key === 'object') {
-      nextState = {
-        ...prevState,
-        ...key,
-      };
-    } else {
-      nextState = {
-        ...prevState,
-        [key]: nextValue
-      };
-    }
+    const nextState: T = {
+      ...prevState,
+      [key]: nextValue
+    };
     
     onChange(nextState);
   };
