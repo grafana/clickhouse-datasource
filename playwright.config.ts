487b554df36172b5cac85a30d61ddfb527783a7e
Adam Yeats <adam.yeats@grafana.com>
2024-08-14 14:59:26 +0100

    tUpdate CI
    Debug
    
    New config

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,10 +26,12 @@ export default defineConfig<PluginOptions>({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: `http://localhost:${process.env.PORT || 3000}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
