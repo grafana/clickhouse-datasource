b6208008d7bbb2b7796cd87f244c6d42d32dd9bd
Adam Yeats <adam.yeats@grafana.com>
2024-10-08 12:35:07 +0100

    Removed unused mock

--- src/components/queryBuilder/QueryBuilder.test.tsx
+++ src/components/queryBuilder/QueryBuilder.test.tsx
@@ -9,14 +9,14 @@ describe('QueryBuilder', () => {
   it('renders correctly', async () => {
     const setState = jest.fn();
     const mockDs = { settings: { jsonData: {} } } as Datasource;
+
     mockDs.fetchDatabases = jest.fn(() => Promise.resolve([]));
     mockDs.fetchTables = jest.fn((_db?: string) => Promise.resolve([]));
     mockDs.fetchColumns = jest.fn(() => {
       setState();
       return Promise.resolve([]);
     });
-    const useStateMock: any = (initState: any) => [initState, setState];
-    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+    
     const result = await waitFor(() =>
       render(
         <QueryBuilder
