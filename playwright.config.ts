8819846f082ed87d6840d38f76d2c565e46bfa57
Adam Yeats <16296989+adamyeats@users.noreply.github.com>
2024-10-29 19:46:46 +0000

    Add Grafana Bench action (#1031)

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,12 +26,16 @@ export default defineConfig<PluginOptions>({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://localhost:${process.env.PORT || 3000}`,
+    baseURL: process.env.GRAFANA_URL || `http://localhost:${process.env.PORT || 3000}`,
+
+    launchOptions: {
+      executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    video: 'on'
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
