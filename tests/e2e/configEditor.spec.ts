5d877ada5e1660f32ce544110d8fd6bdd8f45f76
alyssabull <alyssabull@gmail.com>
2025-08-12 13:45:27 -0600

    test

--- tests/e2e/configEditor.spec.ts
+++ tests/e2e/configEditor.spec.ts
@@ -2,18 +2,19 @@ import { expect, test } from '@grafana/plugin-e2e';
 
 const PLUGIN_UID = 'grafana-clickhouse-datasource';
 const CLICKHOUSE_DB_URL = Boolean(process.env.CI) ? 'clickhouse-server' : 'localhost';
+console.log(process.env)
 
 test.describe('Config Editor', () => {
   test('invalid credentials should return an error', async ({ createDataSourceConfigPage, page }) => {
     const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
-    await page.getByPlaceholder('Server address').fill(CLICKHOUSE_DB_URL);
+    await page.getByPlaceholder('Server address').fill('clickhouse-server');
     await expect(configPage.saveAndTest()).not.toBeOK();
   });
 
   test('valid credentials should display a success alert on the page', async ({ createDataSourceConfigPage, page }) => {
     const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
 
-    await page.getByPlaceholder('Server address').fill(CLICKHOUSE_DB_URL);
+    await page.getByPlaceholder('Server address').fill('clickhouse-server');
     await page.getByPlaceholder('9000').fill('9000');
     await page.getByPlaceholder('default').fill('default');
 
