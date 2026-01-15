e941658a4d7634645f069c34c09a38011addb43a
alyssabull <alyssabull@gmail.com>
2026-01-15 13:13:20 -0700

    Update e2e tests

--- tests/e2e/configEditor.spec.ts
+++ tests/e2e/configEditor.spec.ts
@@ -6,34 +6,20 @@ const CLICKHOUSE_DB_URL = Boolean(process.env.CI) ? 'clickhouse-server' : 'local
 test.describe('Config Editor', () => {
   test('invalid credentials should return an error', async ({ createDataSourceConfigPage, page }) => {
     const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
-    await page.getByPlaceholder('Server address').fill(CLICKHOUSE_DB_URL);
+    await page.getByPlaceholder('Enter server address').fill(CLICKHOUSE_DB_URL);
     await expect(configPage.saveAndTest()).not.toBeOK();
   });
 
   test('valid credentials should display a success alert on the page', async ({ createDataSourceConfigPage, page }) => {
     const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
 
-    await page.getByPlaceholder('Server address').fill(CLICKHOUSE_DB_URL);
-    await page.getByPlaceholder('9000').fill('9000');
-    await page.getByPlaceholder('default').fill('default');
+    await page.getByPlaceholder('Enter server address').fill(CLICKHOUSE_DB_URL);
+    await page.getByPlaceholder('Enter server port').fill('9000');
+    await page.getByPlaceholder('Enter username').fill('default');
 
     await configPage.saveAndTest();
     await expect(configPage).toHaveAlert('success', { hasNotText: 'Datasource updated' });
 
     await page.pause();
-  });
-
-  test('mandatory fields should show error if left empty', async ({ createDataSourceConfigPage, page }) => {
-    const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
-
-    await page.getByPlaceholder('Server address').fill('');
-    await page.keyboard.press('Tab');
-    await expect(page.getByText('Server address required')).toBeVisible();
-
-    await page.getByPlaceholder('9000').fill('');
-    await page.keyboard.press('Tab');
-    await expect(page.getByText('Port is required')).toBeVisible();
-
-    await expect(configPage.saveAndTest()).not.toBeOK();
   });
 });
